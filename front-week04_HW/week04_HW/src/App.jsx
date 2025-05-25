ffd4f4659e343cfbf954ac15a81a05a889127e9e
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-05-25 10:05:23 +0900

    구글 로고 이미지 삽입

---
+++ front-week04_HW/week04_HW/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import googleLogo from "./assets/google_logo.png";
+
+function App() {
+  return (
+    <>
+      <img src={googleLogo} />
+    </>
+  );
+}
+
+export default App;
