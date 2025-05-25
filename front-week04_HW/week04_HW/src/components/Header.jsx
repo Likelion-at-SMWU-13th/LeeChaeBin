54fffb75b2f9b1de9a7f2d5d6e6742a46a4d7d8e
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-05-25 11:27:02 +0900

    Header와 SearchBox 스타일 넣어주기

--- front-week04_HW/week04_HW/src/components/Header.jsx
+++ front-week04_HW/week04_HW/src/components/Header.jsx
@@ -2,16 +2,44 @@ import React from "react";
 import profileImg from "../assets/profile_icon.png";
 import SearchLab from "../assets/SearchLab_icon.png";
 import Apps from "../assets/Apps_icon.png";
+import styled from "styled-components";
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1.5rem;
+  margin-top: 6px;
+  padding: 8px 16px;
+`;
+
+const Text = styled.span`
+  font-size: 13px;
+  cursor: pointer;
+`;
+
+const Icon = styled.img`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+`;
+
+const Profile = styled.img`
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`;
 
 const Header = () => {
   return (
-    <div>
-      <span>Gmail</span>
-      <span>이미지</span>
-      <img src={SearchLab} />
-      <img src={Apps} />
-      <img src={profileImg} />
-    </div>
+    <Card>
+      <Text>Gmail</Text>
+      <Text>이미지</Text>
+      <Icon src={SearchLab} />
+      <Icon src={Apps} />
+      <Profile src={profileImg} />
+    </Card>
   );
 };
 
