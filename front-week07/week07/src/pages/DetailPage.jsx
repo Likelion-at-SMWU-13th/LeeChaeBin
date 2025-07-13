766f7e5d2d2dfcbe44a976ac2ca19c43b55257b1
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-07-13 19:29:58 +0900

    수정하기 기능 추가 완료

--- front-week07/week07/src/pages/DetailPage.jsx
+++ front-week07/week07/src/pages/DetailPage.jsx
@@ -44,7 +44,11 @@ const DetailPage = () => {
         <Time>{detail.timestamp}</Time>
         <Comment>{detail.comment}</Comment>
         <BtnWrapper>
-          <Button txt="수정" fontSize="1.875rem" />
+          <Button
+            txt="수정"
+            fontSize="1.875rem"
+            onBtnClick={() => navigate(`/edit/${detail.id}`)}
+          />
           <Button
             txt="삭제"
             fontSize="1.875rem"
