c6edd924c7a8dfea56ca73f885ed56c6e55b4991
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-07-20 21:09:22 +0900

    Login.jsx 주석처리 완료

--- week08/src/Login.jsx
+++ week08/src/Login.jsx
@@ -3,13 +3,17 @@ import KakaoImg from "./assets/kakao_login.png";
 import GoogleImg from "./assets/google_login.png";
 
 const Login = () => {
+  //Authorization code를 발급받는 단계입니다.
+  //카카오 인증 서버로 이동하도록 합니다.
   const authServerLink = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${
     import.meta.env.VITE_REST_API_KEY
   }&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}`;
+  //카카오 인증 서버를 호출하면서 response_type, client_id, redirect_uri 3가지 쿼리 파라미터를 같이 보냅니다.
 
   const handleKakao = () => {
     window.location.href = authServerLink;
   };
+  //위에서 만든 URL로 이동할 수 있게 하는 함수입니다.
 
   return (
     <div className="login-box">
