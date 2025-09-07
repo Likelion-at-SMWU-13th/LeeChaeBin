4147832c7a90d12a3722fabc687781c3a527e8d0
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-09-07 16:22:24 +0900

    Left 파일 props 제거 및context 사용

--- week2-1_hw/src/components/Left.js
+++ week2-1_hw/src/components/Left.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import { usePart } from "../contexts/PartContext";
 
-const Left = ({ setPart }) => {
+const Left = () => {
+  const { selectPart } = usePart();
+
   return (
     <Wrapper>
-      <button onClick={() => setPart("프론트엔드")}>프론트엔드</button>
-      <button onClick={() => setPart("백엔드")}>백엔드</button>
-      <button onClick={() => setPart("기획/디자인")}>기획/디자인</button>
+      <button onClick={() => selectPart("프론트엔드")}>프론트엔드</button>
+      <button onClick={() => selectPart("백엔드")}>백엔드</button>
+      <button onClick={() => selectPart("기획/디자인")}>기획/디자인</button>
     </Wrapper>
   );
 };
