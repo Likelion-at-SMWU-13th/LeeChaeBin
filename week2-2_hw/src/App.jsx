c7e5ef1656ef4ae6e3cf1b8d632fdb6dd7e6da07
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-09-14 19:37:51 +0900

    App.jsx 작성 완료

--- week2-2_hw/src/App.jsx
+++ week2-2_hw/src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import * as S from "./styles/styled";
+import SongList from "./components/SongList";
+import PlaylistItem from "./components/PlaylistItem";
+import usePlaylistStore from "./store/store";
 
 function App() {
-  return <></>;
+  const playlist = usePlaylistStore((s) => s.playlist);
+
+  return (
+    <S.Page>
+      <SongList />
+      <S.PlaylistBox>
+        {playlist.length === 0 ? (
+          <S.Empty>아직 플레이리스트에 추가된 곡이 없어요!</S.Empty>
+        ) : (
+          playlist.map((song) => <PlaylistItem key={song.id} song={song} />)
+        )}
+      </S.PlaylistBox>
+    </S.Page>
+  );
 }
 
 export default App;
